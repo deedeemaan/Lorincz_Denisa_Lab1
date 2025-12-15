5430f4f795b8618126d01b74a93a68d5df6d9e79
deedeemaan <denisalorincz22@yahoo.com>
2025-12-15 19:13:35 +0200

    modificari

---
+++ Controllers/MyProject.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lorincz_Denisa_Lab1.Controllers
+{
+    public class MyProject : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit în aplicatia mea MVC!";
+        }
+
+        // 2️ Metoda personalizata – salut
+        public string Salut()
+        {
+            return "Salut, utilizator! Sper să ai o zi frumoasă!";
+        }
+
+        // 3️ Metoda cu parametri (string + int)
+        public string Afiseaza(string nume, int varsta)
+        {
+            return $"Salut {nume}, ai {varsta} ani!";
+        }
+    }
+}
