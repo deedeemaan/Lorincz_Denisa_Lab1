d8c4feae20a59737d6e4e0742e5c3880996c0d95
deedeemaan <denisalorincz22@yahoo.com>
2025-12-15 18:35:31 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lorincz_Denisa_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
